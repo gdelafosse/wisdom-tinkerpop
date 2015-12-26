404e0acb36e25e53ba8fe24c212acae9877c94f6
gdelafosse <gdelafosse@axway.com>
2015-12-26 22:00:02 +0100

    add graph details view

--- src/main/java/org/wisdom/tinkerpop/view/GraphController.java
+++ src/main/java/org/wisdom/tinkerpop/view/GraphController.java
@@ -1,11 +1,18 @@
 package org.wisdom.tinkerpop.view;
 
 import java.util.Arrays;
+import java.util.Collection;
 
 import org.apache.felix.ipojo.annotations.Requires;
 import org.apache.tinkerpop.gremlin.structure.Graph;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.framework.ServiceReference;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.wisdom.api.DefaultController;
 import org.wisdom.api.annotations.Controller;
+import org.wisdom.api.annotations.Parameter;
 import org.wisdom.api.annotations.Route;
 import org.wisdom.api.annotations.View;
 import org.wisdom.api.http.HttpMethod;
@@ -18,11 +25,23 @@ import org.wisdom.api.templates.Template;
 @Controller
 public class GraphController extends DefaultController
 {
+    private static final Logger LOGGER = LoggerFactory.getLogger(GraphController.class);
+
     @View("graph-list")
     private Template graphList;
 
+    @View("graph-view")
+    private Template graphView;
+
     @Requires(optional = true)
     private Graph[] graphs;
+
+    private final BundleContext bundleContext;
+
+    public GraphController(final BundleContext bundleContext)
+    {
+        this.bundleContext = bundleContext;
+    }
 
     /**
      * The action returning the graph list.
@@ -30,7 +49,49 @@ public class GraphController extends DefaultController
      * @return the graph list page
      */
     @Route(method = HttpMethod.GET, uri = "/graphs")
-    public Result list() {
+    public Result list()
+    {
         return ok(render(graphList, "graphs", Arrays.stream(graphs).map(g -> new GraphView(g)).toArray()));
+    }
+
+    /**
+     * The action returning the graph view.
+     *
+     * @return the graph detail page
+     */
+    @Route(method = HttpMethod.GET, uri = "/graph/{id}")
+    public Result view(@Parameter("id") String id)
+    {
+        Graph g = findGraph(id);
+        if (g == null)
+        {
+            return notFound();
+        }
+        else
+        {
+            return ok(render(graphView, "graph", new GraphView(g)));
+        }
+    }
+
+    Graph findGraph(String id)
+    {
+        try
+        {
+            Collection<ServiceReference<Graph>> sr = bundleContext.getServiceReferences(Graph.class, "(id=" + id);
+            if (sr.isEmpty())
+            {
+                return null;
+            }
+            else
+            {
+                return bundleContext.getService(sr.stream().findFirst().get());
+            }
+        }
+        catch (InvalidSyntaxException e)
+        {
+            LOGGER.error("Fail to find Graph service {}.", id);
+            LOGGER.error(e.getMessage(), e);
+            return null;
+        }
     }
 }
