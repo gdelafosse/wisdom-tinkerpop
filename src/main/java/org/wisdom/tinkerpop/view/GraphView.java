10bb92f51d4479692b6f3baf8f1abb2c09db99e4
gdelafosse <gdelafosse@axway.com>
2015-12-26 23:27:33 +0100

    add vertex features to detail view

--- src/main/java/org/wisdom/tinkerpop/view/GraphView.java
+++ src/main/java/org/wisdom/tinkerpop/view/GraphView.java
@@ -52,4 +52,41 @@ class GraphView
 
         return graphFeatures;
     }
+
+    public Map<String, Boolean> getEdgeFeatures()
+    {
+        Graph.Features.EdgeFeatures features = graph.features().edge();
+        Map<String, Boolean> edgeFeatures = new HashMap<>();
+        edgeFeatures.put(Graph.Features.EdgeFeatures.FEATURE_ADD_EDGES, features.supportsAddEdges());
+        edgeFeatures.put(Graph.Features.EdgeFeatures.FEATURE_ADD_PROPERTY, features.supportsAddProperty());
+        edgeFeatures.put(Graph.Features.EdgeFeatures.FEATURE_ANY_IDS, features.supportsAnyIds());
+        edgeFeatures.put(Graph.Features.EdgeFeatures.FEATURE_CUSTOM_IDS, features.supportsCustomIds());
+        edgeFeatures.put(Graph.Features.EdgeFeatures.FEATURE_NUMERIC_IDS, features.supportsNumericIds());
+        edgeFeatures.put(Graph.Features.EdgeFeatures.FEATURE_REMOVE_EDGES, features.supportsRemoveEdges());
+        edgeFeatures.put(Graph.Features.EdgeFeatures.FEATURE_REMOVE_PROPERTY, features.supportsRemoveProperty());
+        edgeFeatures.put(Graph.Features.EdgeFeatures.FEATURE_STRING_IDS, features.supportsStringIds());
+        edgeFeatures.put(Graph.Features.EdgeFeatures.FEATURE_USER_SUPPLIED_IDS, features.supportsUserSuppliedIds());
+        edgeFeatures.put(Graph.Features.EdgeFeatures.FEATURE_UUID_IDS, features.supportsUuidIds());
+
+        return edgeFeatures;
+    }
+
+    public Map<String, Boolean> getVertexFeatures()
+    {
+        Graph.Features.VertexFeatures features = graph.features().vertex();
+        Map<String, Boolean> vertexFeatures = new HashMap<>();
+        vertexFeatures.put(Graph.Features.VertexFeatures.FEATURE_ADD_PROPERTY, features.supportsAddProperty());
+        vertexFeatures.put(Graph.Features.VertexFeatures.FEATURE_ADD_VERTICES, features.supportsAddVertices());
+        vertexFeatures.put(Graph.Features.VertexFeatures.FEATURE_ANY_IDS, features.supportsAnyIds());
+        vertexFeatures.put(Graph.Features.VertexFeatures.FEATURE_CUSTOM_IDS, features.supportsCustomIds());
+        vertexFeatures.put(Graph.Features.VertexFeatures.FEATURE_META_PROPERTIES, features.supportsMetaProperties());
+        vertexFeatures.put(Graph.Features.VertexFeatures.FEATURE_MULTI_PROPERTIES, features.supportsMultiProperties());
+        vertexFeatures.put(Graph.Features.VertexFeatures.FEATURE_NUMERIC_IDS, features.supportsNumericIds());
+        vertexFeatures.put(Graph.Features.VertexFeatures.FEATURE_REMOVE_PROPERTY, features.supportsRemoveProperty());
+        vertexFeatures.put(Graph.Features.VertexFeatures.FEATURE_REMOVE_VERTICES, features.supportsRemoveVertices());
+        vertexFeatures.put(Graph.Features.VertexFeatures.FEATURE_STRING_IDS, features.supportsStringIds());
+        vertexFeatures.put(Graph.Features.VertexFeatures.FEATURE_USER_SUPPLIED_IDS, features.supportsUserSuppliedIds());
+        vertexFeatures.put(Graph.Features.VertexFeatures.FEATURE_UUID_IDS, features.supportsUuidIds());
+        return vertexFeatures;
+    }
 }
